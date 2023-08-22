db3b1a6cbeca2e7b04894b448792b323dc3e1799
Lucas <lucascomamala@gmail.com>
2023-08-22 03:03:23 -0600

    Sidebar drawer

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -2,14 +2,27 @@ import { useState } from "react"
 import { Box, useMediaQuery } from "@mui/material"
 import { Outlet } from "react-router-dom"
 import { useSelector } from "react-redux"
+import Sidebar from "components/Sidebar"
 
 import Navbar from 'components/Navbar'
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)")
+  const [isSidebarOpen, setIsSidebarOpen] = useState(isNonMobile)
+
   return (
-    <Box width="100%" height="100%">
+    <Box display={isNonMobile ? "flex" : "block" } width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth='250px'
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-        <Navbar />
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
