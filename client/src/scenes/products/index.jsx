35ed666a63a3a563b4b492dfc9829579187e0eb1
Lucas <lucascomamala@gmail.com>
2023-08-23 22:53:10 -0600

    Products route

---
+++ client/src/scenes/products/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+
+import { useGetProductsQuery } from "state/api"
+
+const Products = () => {
+  return <div>Products</div>;
+};
+
+export default Products;
